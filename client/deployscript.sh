a2469f2f8a61bb777da9fe46b6628deb880af97d
Fede Defaveri <f.g.defaveri@gmail.com>
2019-11-17 22:40:40 +0100

    Fixes typo in deploy script

--- client/deployscript.sh
+++ client/deployscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm run build
-aws s3 cp build/ s3://notarx.com --grants read=uri=http://acs.amazon.com/groups/global/AllUsers --recursive
+aws s3 cp build/ s3://notarx.com --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
