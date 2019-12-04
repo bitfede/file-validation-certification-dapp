44a43f0c2af9c01083eb1d5b25a14a549942db63
Fede Defaveri <f.g.defaveri@gmail.com>
2019-12-04 17:09:25 +0100

    Adds the cache invalidation to the deployscript

--- client/deployscript.sh
+++ client/deployscript.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+DOMAIN="notarx.com"
+
 npm run build
 aws s3 cp build/ s3://notarx.com --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
+
+CLDID=`aws cloudfront list-distributions --query "DistributionList.Items[].{OriginDomainName: Origins.Items[0]DomainName, OriginId: Id }[?contains(OriginDomainName, '$DOMAIN')] | [0]" --output text | cut -f 2`
+sleep 5
+aws cloudfront create-invalidation --distribution-id $CLDID --paths '/*'
