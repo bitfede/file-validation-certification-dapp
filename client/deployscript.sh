141eae01dd9c5f7ccfe2b939d263252d9c6356b2
bitfede <f.g.defaveri@gmail.com>
2019-12-04 17:30:23 +0100

    Fixes bug in the deployscript

--- client/deployscript.sh
+++ client/deployscript.sh
@@ -5,6 +5,6 @@ DOMAIN="notarx.com"
 npm run build
 aws s3 cp build/ s3://notarx.com --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
 
-CLDID=`aws cloudfront list-distributions --query "DistributionList.Items[].{OriginDomainName: Origins.Items[0]DomainName, OriginId: Id }[?contains(OriginDomainName, '$DOMAIN')] | [0]" --output text | cut -f 2`
+CLDID=`aws cloudfront list-distributions --query "DistributionList.Items[].{OriginDomainName: Origins.Items[0].DomainName, OriginId: Id }[?contains(OriginDomainName, '$DOMAIN')] | [0]" --output text | cut -f 2`
 sleep 5
 aws cloudfront create-invalidation --distribution-id $CLDID --paths '/*'
